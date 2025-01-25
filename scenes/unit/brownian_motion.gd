adf1514f636c0efc78b8d83766d8b4f7cd9c2800
s7eamy <aurimasotaku225@gmail.com>
2025-01-25 15:26:32 +0200

    fix: fix brownian motion going up or down

--- scenes/unit/brownian_motion.gd
+++ scenes/unit/brownian_motion.gd
@@ -1,13 +1,13 @@
-extends CharacterBody3D
+extends Node3D
 
 # Speed of the Brownian motion
-export var speed: float = 1.0
+@export var speed: float = 1.0
 
 func _process(delta: float) -> void:
-    var random_direction = Vector3(
-        randf() - 0.5,
-        randf() - 0.5,
-        randf() - 0.5
-    ).normalized()
-    var movement = random_direction * speed * delta
-    translate(movement)
+	var random_direction = Vector3(
+		randf() - 0.5,
+		0, # should not move up or down
+		randf() - 0.5
+	).normalized()
+	var movement = random_direction * speed * delta
+	translate(movement)
