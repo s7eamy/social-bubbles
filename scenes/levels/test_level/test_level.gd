c6dec95107a0ea4c8dd086bcc24d5cbd08b942b7
s7eamy <aurimasotaku225@gmail.com>
2025-01-25 17:48:05 +0200

    feat: add random unit generation in test level

---
+++ scenes/levels/test_level/test_level.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var unit_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    randomize()
+    var num_units = randi_range(3, 8)
+    for i in range(num_units):
+        var unit_instance = unit_scene.instantiate()
+        unit_instance.position = Vector2(
+            randf_range(0, get_viewport_rect().size.x),
+            randf_range(0, get_viewport_rect().size.y)
+        )
+        add_child(unit_instance)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
