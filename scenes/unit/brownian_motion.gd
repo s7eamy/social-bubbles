16a9d64719de8601901b400c6f9310216096e256
s7eamy <aurimasotaku225@gmail.com>
2025-01-25 16:38:28 +0200

    feat: implement unit outlines

--- scenes/unit/brownian_motion.gd
+++ scenes/unit/brownian_motion.gd
@@ -1,13 +1,21 @@
-extends Node3D
+extends CharacterBody2D
 
 # Speed of the Brownian motion
-@export var speed: float = 1.0
+@export var speed: float = 350.0
+# Time interval to update direction
+@export var update_interval: float = 0.2
+
+var time_since_last_update: float = 0.0
+var current_direction: Vector2 = Vector2.ZERO
 
 func _process(delta: float) -> void:
-	var random_direction = Vector3(
-		randf() - 0.5,
-		0, # should not move up or down
-		randf() - 0.5
-	).normalized()
-	var movement = random_direction * speed * delta
-	translate(movement)
+    time_since_last_update += delta
+    if time_since_last_update >= update_interval:
+        time_since_last_update = 0.0
+        current_direction = Vector2(
+            randf() - 0.5,
+            randf() - 0.5
+        ).normalized()
+    
+    velocity = current_direction * speed
+    move_and_slide()
